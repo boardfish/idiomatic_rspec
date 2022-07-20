4aa711f2ca8df9c2fc3c7bd31cb5bff32f8ab138
Simon Fish <si@mon.fish>
2022-07-20 10:21:39 +0100

    Extract setup out to shared contexts
    To avoid nesting contexts, we can extract out the associated setup. In
    this particular case, it might feel redundant at first since we're only
    running one test per context, but that could change in future. A better
    example to illustrate this would be an ActiveRecord model - you might
    want to test that the value of an attribute has changed, whether the
    record has been persisted, and whether the record is valid, for example:
    
    it { is_expected.not_to be_persisted }
    it { is_expected.not_to be_valid }
    it { is_expected.to have_attributes(name: "Daiji Igarashi") }

--- spec/guitar_spec.rb
+++ spec/guitar_spec.rb
@@ -5,84 +5,108 @@ RSpec.describe Guitar do
   describe "#strum" do
     subject { guitar.strum(string) }
 
-    context 'when tuning is default' do
+    shared_context 'when tuning is default' do
       let(:guitar) { Guitar.new }
-
-      context 'when the first string is strummed' do
-        let(:string) { 0 }
-
-        it { is_expected.to eq('E2') }
-      end
-
-      context 'when the second string is strummed' do
-        let(:string) { 1 }
-
-        it { is_expected.to eq('A2') }
-      end
-
-      context 'when the third string is strummed' do
-        let(:string) { 2 }
-
-        it { is_expected.to eq('D3') }
-      end
-
-      context 'when the fourth string is strummed' do
-        let(:string) { 3 }
-
-        it { is_expected.to eq('G3') }
-      end
-
-      context 'when the fifth string is strummed' do
-        let(:string) { 4 }
-
-        it { is_expected.to eq('B3') }
-      end
-
-      context 'when the sixth string is strummed' do
-        let(:string) { 5 }
-
-        it { is_expected.to eq('E4') }
-      end
     end
 
-    context 'when tuning is drop D' do
+    shared_context 'when tuning is drop D' do
       let(:guitar) { Guitar.new(tuning: { 0 => -2 }) }
+    end
 
-      context 'when the first string is strummed' do
-        let(:string) { 0 }
+    context 'when the first string is strummed in default tuning' do
+      include_context 'when tuning is default'
 
-        it { is_expected.to eq('D2') }
-      end
+      let(:string) { 0 }
 
-      context 'when the second string is strummed' do
-        let(:string) { 1 }
+      it { is_expected.to eq('E2') }
+    end
 
-        it { is_expected.to eq('A2') }
-      end
+    context 'when the second string is strummed in default tuning' do
+      include_context 'when tuning is default'
 
-      context 'when the third string is strummed' do
-        let(:string) { 2 }
+      let(:string) { 1 }
 
-        it { is_expected.to eq('D3') }
-      end
+      it { is_expected.to eq('A2') }
+    end
 
-      context 'when the fourth string is strummed' do
-        let(:string) { 3 }
+    context 'when the third string is strummed in default tuning' do
+      include_context 'when tuning is default'
 
-        it { is_expected.to eq('G3') }
-      end
+      let(:string) { 2 }
 
-      context 'when the fifth string is strummed' do
-        let(:string) { 4 }
+      it { is_expected.to eq('D3') }
+    end
 
-        it { is_expected.to eq('B3') }
-      end
+    context 'when the fourth string is strummed in default tuning' do
+      include_context 'when tuning is default'
 
-      context 'when the sixth string is strummed' do
-        let(:string) { 5 }
+      let(:string) { 3 }
 
-        it { is_expected.to eq('E4') }
-      end
+      it { is_expected.to eq('G3') }
+    end
+
+    context 'when the fifth string is strummed in default tuning' do
+      include_context 'when tuning is default'
+
+      let(:string) { 4 }
+
+      it { is_expected.to eq('B3') }
+    end
+
+    context 'when the sixth string is strummed in default tuning' do
+      include_context 'when tuning is default'
+
+      let(:string) { 5 }
+
+      it { is_expected.to eq('E4') }
+    end
+
+    context 'when the first string is strummed in drop D tuning' do
+      include_context 'when tuning is drop D'
+
+      let(:string) { 0 }
+
+      it { is_expected.to eq('D2') }
+    end
+
+    context 'when the second string is strummed in drop D tuning' do
+      include_context 'when tuning is drop D'
+
+      let(:string) { 1 }
+
+      it { is_expected.to eq('A2') }
+    end
+
+    context 'when the third string is strummed in drop D tuning' do
+      include_context 'when tuning is drop D'
+
+      let(:string) { 2 }
+
+      it { is_expected.to eq('D3') }
+    end
+
+    context 'when the fourth string is strummed in drop D tuning' do
+      include_context 'when tuning is drop D'
+
+      let(:string) { 3 }
+
+      it { is_expected.to eq('G3') }
+    end
+
+    context 'when the fifth string is strummed in drop D tuning' do
+      include_context 'when tuning is drop D'
+
+      let(:string) { 4 }
+
+      it { is_expected.to eq('B3') }
+    end
+
+    context 'when the sixth string is strummed in drop D tuning' do
+      include_context 'when tuning is drop D'
+
+      let(:string) { 5 }
+
+      it { is_expected.to eq('E4') }
     end
   end
 end
