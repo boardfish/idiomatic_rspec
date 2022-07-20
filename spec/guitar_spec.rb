ab01107cb0b97e84316e264e75439760a0a0390f
Simon Fish <si@mon.fish>
2022-07-20 09:23:57 +0100

    Refactor: use #let, #context, and #describe
    context - We've identified that the default tuning and the drop D tuning
    are two changes we're making that are common to a group of tests. This
    forms a context.
    let - We're also repeatedly declaring Guitar.new between these tests.
    Using let gives us a block that gets rerun with each test and is memoized.
    describe - We're also only calling the #strum method and testing it in
    each of these conditions, so we should surround these tests in a #describe
    block so that if there's another method on Guitar, we can test it separately.

--- spec/guitar_spec.rb
+++ spec/guitar_spec.rb
@@ -2,51 +2,61 @@ require 'spec_helper'
 require_relative '../guitar'
 
 RSpec.describe Guitar do
-  it 'produces E2 when the first string is strummed' do
-    expect(Guitar.new.strum(0)).to eq('E2')
-  end
+  describe "#strum" do
+    context 'when tuning is default' do
+      let(:guitar) { Guitar.new }
 
-  it 'produces A2 when the second string is strummed' do
-    expect(Guitar.new.strum(1)).to eq('A2')
-  end
+      it 'produces E2 when the first string is strummed' do
+        expect(guitar.strum(0)).to eq('E2')
+      end
 
-  it 'produces D3 when the third string is strummed' do
-    expect(Guitar.new.strum(2)).to eq('D3')
-  end
+      it 'produces A2 when the second string is strummed' do
+        expect(guitar.strum(1)).to eq('A2')
+      end
 
-  it 'produces G3 when the fourth string is strummed' do
-    expect(Guitar.new.strum(3)).to eq('G3')
-  end
+      it 'produces D3 when the third string is strummed' do
+        expect(guitar.strum(2)).to eq('D3')
+      end
 
-  it 'produces B3 when the fifth string is strummed' do
-    expect(Guitar.new.strum(4)).to eq('B3')
-  end
+      it 'produces G3 when the fourth string is strummed' do
+        expect(guitar.strum(3)).to eq('G3')
+      end
 
-  it 'produces E4 when the sixth string is strummed' do
-    expect(Guitar.new.strum(4)).to eq('E4')
-  end
+      it 'produces B3 when the fifth string is strummed' do
+        expect(guitar.strum(4)).to eq('B3')
+      end
 
-  it 'produces D2 when the first string is strummed and tuning is drop D' do
-    expect(Guitar.new(tuning: { 0 => -2 }).strum(0)).to eq('D2')
-  end
+      it 'produces E4 when the sixth string is strummed' do
+        expect(guitar.strum(5)).to eq('E4')
+      end
+    end
 
-  it 'produces A2 when the second string is strummed and tuning is drop D' do
-    expect(Guitar.new(tuning: { 0 => -2 }).strum(1)).to eq('A2')
-  end
+    context 'when tuning is drop D' do
+      let(:guitar) { Guitar.new(tuning: { 0 => -2 }) }
 
-  it 'produces D3 when the third string is strummed and tuning is drop D' do
-    expect(Guitar.new(tuning: { 0 => -2 }).strum(2)).to eq('D3')
-  end
+      it 'produces D2 when the first string is strummed' do
+        expect(guitar.strum(0)).to eq('D2')
+      end
 
-  it 'produces G3 when the fourth string is strummed and tuning is drop D' do
-    expect(Guitar.new(tuning: { 0 => -2 }).strum(3)).to eq('G3')
-  end
+      it 'produces A2 when the second string is strummed' do
+        expect(guitar.strum(1)).to eq('A2')
+      end
 
-  it 'produces B3 when the fifth string is strummed and tuning is drop D' do
-    expect(Guitar.new(tuning: { 0 => -2 }).strum(4)).to eq('B3')
-  end
+      it 'produces D3 when the third string is strummed' do
+        expect(guitar.strum(2)).to eq('D3')
+      end
 
-  it 'produces E4 when the sixth string is strummed and tuning is drop D' do
-    expect(Guitar.new(tuning: { 0 => -2 }).strum(5)).to eq('E4')
+      it 'produces G3 when the fourth string is strummed' do
+        expect(guitar.strum(3)).to eq('G3')
+      end
+
+      it 'produces B3 when the fifth string is strummed' do
+        expect(guitar.strum(4)).to eq('B3')
+      end
+
+      it 'produces E4 when the sixth string is strummed' do
+        expect(guitar.strum(5)).to eq('E4')
+      end
+    end
   end
 end
