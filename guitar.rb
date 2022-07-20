5664a9be87f062c1dd66e41b01ac11a57d561960
Simon Fish <si@mon.fish>
2022-07-20 10:27:41 +0100

    Example implementation
    This satisfies all but one of the tests we have:
    
    ```
    bundle exec rspec --format documentation                                                                                                                                                                                                                                                                                                        [10:18:56]
    
    Guitar
      #strum
        when the first string is strummed in default tuning
          is expected to eq "E2"
        when the second string is strummed in default tuning
          is expected to eq "A2"
        when the third string is strummed in default tuning
          is expected to eq "D3"
        when the fourth string is strummed in default tuning
          is expected to eq "G3"
        when the fifth string is strummed in default tuning
          is expected to eq "B3"
        when the sixth string is strummed in default tuning
          is expected to eq "E4"
        when the first string is strummed in drop D tuning
          is expected to eq "D2" (FAILED - 1)
        when the second string is strummed in drop D tuning
          is expected to eq "A2"
        when the third string is strummed in drop D tuning
          is expected to eq "D3"
        when the fourth string is strummed in drop D tuning
          is expected to eq "G3"
        when the fifth string is strummed in drop D tuning
          is expected to eq "B3"
        when the sixth string is strummed in drop D tuning
          is expected to eq "E4"
    
    Failures:
    
      1) Guitar#strum when the first string is strummed in drop D tuning is expected to eq "D2"
         Failure/Error: it { is_expected.to eq('D2') }
    
           expected: "D2"
                got: "E2"
    
           (compared using ==)
         # ./spec/guitar_spec.rb:69:in `block (4 levels) in <top (required)>'
    
    Finished in 0.00819 seconds (files took 0.05594 seconds to load)
    12 examples, 1 failure
    
    Failed examples:
    
    rspec ./spec/guitar_spec.rb:69 # Guitar#strum when the first string is strummed in drop D tuning is expected to eq "D2"
    ```

--- guitar.rb
+++ guitar.rb
@@ -1,3 +1,20 @@
 class Guitar
-    
+  DEFAULT_TUNING = %w[E2 A2 D3 G3 B3 E4].freeze
+
+  def initialize(tuning: Hash.new(0))
+    @string_pitches = DEFAULT_TUNING
+    tune(tuning)
+  end
+
+  def strum(string)
+    string_pitches.fetch(string)
+  end
+
+  private
+
+  attr_accessor :string_pitches
+
+  def tune(tuning)
+    # Unimplemented
+  end
 end
