7486252dc67cefd3eebdafef1fee59a83b842423
Simon Fish <si@mon.fish>
2022-07-20 09:49:21 +0100

    Use `subject` to fully extract setup outside tests
    Since we're describing the `strum` method, it makes sense that the subject
    of each test should be a call to the `strum` method. `subject` works identically to `let`.
    Unless you pass it a name, you'll find the variable `subject` is now usable in your tests.
    `subject` has an added bonus to `let`, though - if you fully commit to having all your setup
    outside the body of the test, it lets you declare clean one-liner tests of the form it { is_expected.to ... }.
    You can use any RSpec matcher with this.
    
    At this point, we're also using nested contexts, though, which is
    advised against. By default, rubocop-rspec will warn you if you're nesting
    contexts multiple levels deep, as this can reduce readability.

--- spec/guitar_spec.rb
+++ spec/guitar_spec.rb
@@ -3,59 +3,85 @@ require_relative '../guitar'
 
 RSpec.describe Guitar do
   describe "#strum" do
+    subject { guitar.strum(string) }
+
     context 'when tuning is default' do
       let(:guitar) { Guitar.new }
 
-      it 'produces E2 when the first string is strummed' do
-        expect(guitar.strum(0)).to eq('E2')
+      context 'when the first string is strummed' do
+        let(:string) { 0 }
+
+        it { is_expected.to eq('E2') }
       end
 
-      it 'produces A2 when the second string is strummed' do
-        expect(guitar.strum(1)).to eq('A2')
+      context 'when the second string is strummed' do
+        let(:string) { 1 }
+
+        it { is_expected.to eq('A2') }
       end
 
-      it 'produces D3 when the third string is strummed' do
-        expect(guitar.strum(2)).to eq('D3')
+      context 'when the third string is strummed' do
+        let(:string) { 2 }
+
+        it { is_expected.to eq('D3') }
       end
 
-      it 'produces G3 when the fourth string is strummed' do
-        expect(guitar.strum(3)).to eq('G3')
+      context 'when the fourth string is strummed' do
+        let(:string) { 3 }
+
+        it { is_expected.to eq('G3') }
       end
 
-      it 'produces B3 when the fifth string is strummed' do
-        expect(guitar.strum(4)).to eq('B3')
+      context 'when the fifth string is strummed' do
+        let(:string) { 4 }
+
+        it { is_expected.to eq('B3') }
       end
 
-      it 'produces E4 when the sixth string is strummed' do
-        expect(guitar.strum(5)).to eq('E4')
+      context 'when the sixth string is strummed' do
+        let(:string) { 5 }
+
+        it { is_expected.to eq('E4') }
       end
     end
 
     context 'when tuning is drop D' do
       let(:guitar) { Guitar.new(tuning: { 0 => -2 }) }
 
-      it 'produces D2 when the first string is strummed' do
-        expect(guitar.strum(0)).to eq('D2')
+      context 'when the first string is strummed' do
+        let(:string) { 0 }
+
+        it { is_expected.to eq('D2') }
       end
 
-      it 'produces A2 when the second string is strummed' do
-        expect(guitar.strum(1)).to eq('A2')
+      context 'when the second string is strummed' do
+        let(:string) { 1 }
+
+        it { is_expected.to eq('A2') }
       end
 
-      it 'produces D3 when the third string is strummed' do
-        expect(guitar.strum(2)).to eq('D3')
+      context 'when the third string is strummed' do
+        let(:string) { 2 }
+
+        it { is_expected.to eq('D3') }
       end
 
-      it 'produces G3 when the fourth string is strummed' do
-        expect(guitar.strum(3)).to eq('G3')
+      context 'when the fourth string is strummed' do
+        let(:string) { 3 }
+
+        it { is_expected.to eq('G3') }
       end
 
-      it 'produces B3 when the fifth string is strummed' do
-        expect(guitar.strum(4)).to eq('B3')
+      context 'when the fifth string is strummed' do
+        let(:string) { 4 }
+
+        it { is_expected.to eq('B3') }
       end
 
-      it 'produces E4 when the sixth string is strummed' do
-        expect(guitar.strum(5)).to eq('E4')
+      context 'when the sixth string is strummed' do
+        let(:string) { 5 }
+
+        it { is_expected.to eq('E4') }
       end
     end
   end
